cd02ac1c66b3ebe9b074f3ef10234e6078a6623f
ChooChye <37932895+ChooChye@users.noreply.github.com>
2020-09-02 21:09:54 +0800

    Sensors data is retrievable

--- app/src/main/java/com/example/ihome/security/Security.kt
+++ app/src/main/java/com/example/ihome/security/Security.kt
@@ -4,29 +4,28 @@ import android.content.Context
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ihome.R
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_security.*
 import java.io.*
 import java.lang.StringBuilder
 import java.time.LocalDateTime
 import java.util.*
+import java.text.SimpleDateFormat
 import kotlin.collections.ArrayList
+import kotlin.math.floor
 
 
 class Security : AppCompatActivity() {
     private val FILE_NAME = "example.txt"
 
-    var database = FirebaseDatabase.getInstance()
-    val myRef = database.getReference("PI_01_CONTROL") //PI_01_CONTROL
-    //private var timeArr: ArrayList<reportTime> ? = null
+    private var database = FirebaseDatabase.getInstance()
+    private val myRef = database.getReference("PI_01_CONTROL") //PI_01_CONTROL
+    private val date: String = SimpleDateFormat("yyyyMMdd", Locale.getDefault()).format(Date())
+    private val hour: String = SimpleDateFormat("HH", Locale.getDefault()).format(Date())
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,13 +34,11 @@ class Security : AppCompatActivity() {
         var actionBar:ActionBar = supportActionBar!!
         actionBar.title = "Security"
         actionBar.setDisplayHomeAsUpEnabled(true)
-        //security_textView_report.text = currentTime.toString()
-        //check if buzzer is turned on
-        checkAlarm()
-        //load()
+
+        checkAlarm()  //check if buzzer is turned on
 
         security_switch_alarm.setOnCheckedChangeListener({ _ , isChecked ->
-            val message = if (isChecked) {
+            if (isChecked) {
                 alarmController(1)
                 now()
             }else{
@@ -57,16 +54,14 @@ class Security : AppCompatActivity() {
     }
 
     private fun checkAlarm(){
-
         myRef.addValueEventListener(object : ValueEventListener{
             override fun onCancelled(p0: DatabaseError) {
-                Log.d("TestingSecurity_READ", "${p0.toException()}")
+                //Log.d("TestingSecurity_READ", "${p0.toException()}")
             }
-
             override fun onDataChange(p0: DataSnapshot) {
                 if(p0.exists()){
                     val map: Map<String, Object> = p0.getValue() as Map<String, Object>
-                    Log.d("TestingSecurity_Buzz", "Value is: " + map["buzzer"]);
+                    //Log.d("TestingSecurity_Buzz", "Value is: " + map["buzzer"]);
                     if(map["buzzer"].toString() == 1.toString()){
                         //if buzzer == 1 turn on switch
                         security_switch_alarm.isChecked = true
@@ -126,6 +121,27 @@ class Security : AppCompatActivity() {
 
             }
         }
+    }
+
+    private fun getSensorData(){
+        val date = "PI_01_"+date
+        var myRefSens = database.getReference(date).child(hour).orderByKey().limitToLast(1)
+
+        myRefSens.addValueEventListener(object : ValueEventListener{
+            override fun onCancelled(p0: DatabaseError) {
+                Log.d("TestingSecurity_Sensor", "${p0.toException()}")
+            }
+            override fun onDataChange(p0: DataSnapshot) {
+                if(p0.exists()){
+                    val child = p0.children
+                    for (data in child){
+                        //change the path variable
+                        Log.d("TestingSecurity_Sensor", data.child("ultra").getValue().toString())
+                    }
+                    //security_textView_report.text = child.toString()
+                }
+            }
+        })
     }
 
     override fun onSupportNavigateUp(): Boolean {
